30f3756b2ac934190e89b5ee501c7d13165f1d17
nomaterials <nomaterials@gmail.com>
2024-08-19 16:34:21 +0200

    expose dev shells from roof flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,37 @@
 {
   description = "A collection of flake templates";
 
-  outputs = { self }: {
-    templates = {
-      rust = {
-        path = ./rust;
-        description = "Rust template, using Fenix";
-      };
-      bevy = {
-        path = ./bevy;
-        description = "Bevy template, using stable Fenix";
-      };
+  inputs = {
+    rust = {
+      url = "path:./rust";
     };
+    bevy = {
+      url = "path:./bevy";
+    };
+  };
 
-    defaultTemplate = self.templates.rust;
+  outputs = { self, rust, bevy }:
+  let
+    system = "x86_64-linux"; # Adjust if necessary
+  in {
+      
+  # Templates definition
+  templates = {
+    rust = {
+      path = ./rust;
+      description = "Rust template, using Fenix";
+    };
+    bevy = {
+      path = ./bevy;
+      description = "Bevy template, using stable Fenix";
+    };
+  };
+
+  # Set the default template
+  defaultTemplate = self.templates.rust;
+
+  # Dev shells
+  rust = rust.devShells.${system}.default;
+  bevy = bevy.devShells.${system}.default;
   };
 }
